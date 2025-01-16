7ad4dd26b3158e97b9fba1c1a49f514dc5a3ad35
Hugo Enrique Calderon <hugocalderongiron@gmail.com>
2025-01-16 05:31:31 -0500

    fix components

--- docker/init.sh
+++ docker/init.sh
@@ -2,13 +2,20 @@
 # docker/init.sh
 
 # Verificar si el proyecto ya existe
-if [ ! -f "composer.json" ]; then
+if [ ! -f "/var/www/html/composer.json" ]; then
     echo "Creando nuevo proyecto Laravel..."
-    composer create-project --prefer-dist laravel/laravel .
+    composer create-project --prefer-dist laravel/laravel html
+    echo "-> Inicializando...."
+    cd html
+    echo "Instalando dependencias"
     composer install
     php artisan key:generate
+    echo "Instalacion completada"
 else
     echo "El proyecto ya existe, iniciando servicios..."
+    echo "-> Inicializando...."
+    cd html
+    echo "Actualizando componentes"
     composer install
 fi
 
